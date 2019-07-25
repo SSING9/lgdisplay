e634e798d7f7049eea7741b4108e67321ad9dc2a
SSING9 <shk9033@gmail.com>
2019-07-25 11:37:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
+/* 메뉴 슬라이드  */
+$('#mainNav > ul > li').mouseover(function(){
+    $(this).find('.subNavWrap').stop().slideDown(400);
+});
+$('#mainNav > ul > li').mouseleave(function(){
+    $(this).find('.subNavWrap').stop().slideUp(100);
+});
+
+/* 이미지 슬라이드 */
+
 $('.imgSlide').slick({
    dots: true,
    autoplay: true,
    autoplaySpeed: 3000,
    arrows: false,
-   responsive: [
-       {
-           breakpoint: 768,
-           settings: {
-               arrows: true,
-               dots: false
-           }
-       }
-   ]
 });
 
-/* 이미지 슬라이드 끝 */
+/* 탑버튼 */
+$('.topBtn').hide();
+$(window).scroll(function(){
+    if($(this).scrollTop() > $('#product').scrollTop()){
+        $('.topBtn').fadeIn();
+    } else {
+        $('.topBtn').fadeOut();
+    }
+});
